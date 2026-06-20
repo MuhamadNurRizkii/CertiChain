4264a61f0c922f08749f01528a78321bbcfa8a82
Muhamad Nur Rizki <farianhab123@gmail.com>
2026-06-20 22:46:29 +0700

    memperbaiki posisi qr

--- server/src/services/pdf.service.js
+++ server/src/services/pdf.service.js
@@ -12,8 +12,8 @@ export const embedQR = async (pdf, url) => {
   const page = pdfDoc.getPages()[0];
 
   page.drawImage(qrImage, {
-    x: 600,
-    y: 100,
+    x: 450,
+    y: 40,
     width: 100,
     height: 100,
   });
